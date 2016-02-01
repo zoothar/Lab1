3c4958cb4883e4a07a0886bd5904a601c9040500
zoothar <cory.gomez541@myci.csuci.edu>
2016-02-01 13:32:13 -0800

    Second try since first failed

---
+++ StudentView.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comp350_lab1;
+
+/**
+ *
+ * @author batman
+ */
+public class StudentView {
+    
+    public void printStudentDetails(String studentName, String studentRollNo){
+      System.out.println("Student: ");
+      System.out.println("Name: " + studentName);
+      System.out.println("Roll No: " + studentRollNo);
+   }
+    
+}
